fa4fd4b4853d16ca09db534e72a2fb27c33718f6
henrymbuguak <henrymbuguak@gmail.com>
2018-03-25 18:38:59 +0300

    writing user register test

---
+++ test_UserRegister.py
@@ -0,0 +1,13 @@
+import unittest
+from run import *
+from resources import UserRegistration
+
+
+class TestUserRegistration(unittest.TestCase):
+    def test_register(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+
