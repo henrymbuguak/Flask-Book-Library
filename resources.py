899238a9dbae8ae546b4634c657c937116d49c3f
henrymbuguak <henrymbuguak@gmail.com>
2018-03-29 13:35:00 +0300

    Removing db configuration from the project

--- resources.py
+++ resources.py
@@ -13,104 +13,42 @@ parser.add_argument('username', help='This field cannot be blank', required=True
 parser.add_argument('password', help='This field cannot be blank', required=True)
 
 
-class UserRegistration(Resource):
+class UserRegister(Resource):
     def post(self):
-        data = parser.parse_args()
-        if UserModel.find_by_username(data['username']):
-            return {'message': 'Username {} already exist'.format(data['username'])}
-
-        new_user = UserModel(
-            username=data['username'],
-            password=UserModel.generate_hash(data['password']),
-        )
-        try:
-            # new_user.save_to_db()
-            registered_users.append(new_user)
-            access_token = create_access_token(identity=data['username'])
-            refresh_token = create_refresh_token(identity=data['username'])
-            return {
-                'message': 'User {} was created'.format(data['username']),
-                'access_token': access_token,
-                'refresh_token': refresh_token
-            }
-        except:
-            return {'message': 'Something went wrong'}, 500
+        username = request.form['username']
+        password = request.form['password']
+        registered_users.append(username)
+        registered_users.append(password)
+        access_token = create_access_token(identity=['username'])
+        return {
+            'username': username,
+            'access_token': access_token
+        }
 
 
 class UserLogin(Resource):
     def post(self):
-        data = parser.parse_args()
-        current_user = UserModel.find_by_username(data['username'])
+        username= request.form['username']
+        password= request.form['password']
+        current_user = username in registered_users
+        print(current_user)
         if not current_user:
-            return {'message': 'User {} does not exist'.format(data['username'])}
-
-        if UserModel.verify_hash(data['password'], current_user.password):
-            access_token = create_access_token(identity=data['username'])
-            refresh_token = create_refresh_token(identity=data['username'])
-            return {
-                'message': 'You are logged in as {}'.format(current_user.username),
-                'access_token': access_token,
-                'refresh_token': refresh_token
-            }
-        else:
-            return {'message': 'Wrong password or username'}
+            return {'message': 'User does not exist'}
+        return {
+            'message': 'You are logged in as {}',
+        }
 
 
-class UserLogoutAccess(Resource):
-    @jwt_required
+class UserLogout(Resource):
     def post(self):
-        jti = get_raw_jwt()['jti']
-        try:
-            revoked_token = RevokedTokenModel(jti=jti)
-            revoked_token.add()
-            return {'message': 'Access token has been revoked!'}
-        except:
-            return {'message': 'Something went wrong'}, 500
-
-
-class UserLogoutRefresh(Resource):
-    @jwt_refresh_token_required
-    def post(self):
-        jti = get_raw_jwt()['jti']
-        try:
-            revoked_token = RevokedTokenModel(jti=jti)
-            revoked_token.add()
-            return {'message': 'Refresh token has been revoked!'}
-        except:
-            return {'message': 'Something went wrong'}, 500
-
-
-class TokenRefresh(Resource):
-    @jwt_refresh_token_required
-    def post(self):
-        current_user = get_jwt_identity()
-        access_token = create_access_token(identity=current_user)
-        return {'access_token': access_token}
-
-
-class UserPasswordReset(Resource):
-    def post(self):
-        def post(self):
-            data = parser.parse_args()
-            user = UserModel.find_by_username(data['username'])
-            if not user:
-                return {'message': 'User {} does not exist'.format(data['username'])}
-            return {'message': 'To reset password follow this link'}
+        return {
+            'message': 'logout successfully'
+        }
 
 
 class AllUsers(Resource):
     def get(self):
         return jsonify(registered_users=registered_users)
-        # return UserModel.return_all()
-
-    def delete(self):
-        return UserModel.delete_all()
-
-
-class SecretResource(Resource):
-    @jwt_required
-    def get(self):
-        return {'answer': 43}
 
 
 class CreateBook(Resource):
@@ -163,7 +101,5 @@ class GetAllBooks(Resource):
 
 class BorrowBook(Resource):
     def post(self, book_id):
-        args = parser.parse_args()
-        book = {'book': args['book']}
-        add_book[book_id] = book
+
         return add_book
--- run.py
+++ run.py
@@ -6,17 +6,17 @@ from flask_jwt_extended import JWTManager
 app = Flask(__name__)
 api = Api(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY'] = '8ur$*&ebeugxsg%3l1^2^g-(5tfh2+%v#usbb=4h$wemlprv07'
 
 
-db = SQLAlchemy(app)
-
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
+# db = SQLAlchemy(app)
+#
+#
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
 
 
 app.config['JWT_SECRET_KEY'] = '9ur$*&ebeugxsg%3l1^2^g-(5tfh2+%v#usbb=4h$wemlprv09'
@@ -39,9 +39,9 @@ registered_users = []
 import views, models, resources
 
 
-api.add_resource(resources.UserRegistration, '/api/auth/register')
+api.add_resource(resources.UserRegister, '/api/auth/register')
 api.add_resource(resources.UserLogin, '/api/auth/login')
-api.add_resource(resources.UserLogoutAccess, '/api/auth/logout')
+api.add_resource(resources.UserLogout, '/api/auth/logout')
 api.add_resource(resources.UserLogoutRefresh, '/logout/refresh')
 api.add_resource(resources.TokenRefresh, '/token/refresh')
 api.add_resource(resources.AllUsers, '/users')
